ca6a1f0810815fe981dc06f04ae095a73fdccf08
hongoh <hg@kxc.io>
2022-03-11 00:17:49 +0900

    add jsonm encoder generator for record type_decl

--- bin/main.ml
+++ bin/main.ml
@@ -30,52 +30,77 @@ module Datatype_desc = struct
     | Record_kind of record_type_desc
     | Variant_kind of variant_type_desc
 
-  let attributes_of_doc = function
-    | `docstr doc ->
-       let loc = Location.none in
-       [attribute ~loc
-          ~name:(Located.mk ~loc "ocaml.doc")
-          ~payload:(PStr [pstr_eval ~loc (estring ~loc doc) []])]
-    | `nodoc -> []
-    | _ -> failwith "unknown polymorphic variant for docstr"
-
-  let kind_type_of_record_type_desc : record_type_desc -> type_kind = fun fields ->
-    let loc = Location.none in
-    Ptype_record (List.map (fun ({rf_name; rf_type;}, doc) ->
-        { (label_declaration ~loc ~mutable_:Immutable
-             ~name:(Located.mk ~loc rf_name)
-             ~type_:(ptyp_constr ~loc (Located.mk ~loc (lident rf_type)) []))
-          with pld_attributes = attributes_of_doc doc; })
-        fields)
-
-  let kind_type_of_variant_type_desc : variant_type_desc -> type_kind = fun constrs ->
-    let loc = Location.none in
-    Ptype_variant (List.map (function
-        | Cstr_tuple {ct_name; ct_args;}, doc ->
-           { (constructor_declaration ~loc ~res:None
-                ~name:(Located.mk ~loc ct_name)
-                ~args:(Pcstr_tuple (List.map (fun ct_arg ->
-                    ptyp_constr ~loc (Located.mk ~loc (lident ct_arg)) [])
-                    ct_args)))
-             with pcd_attributes = attributes_of_doc doc; }
-        | Cstr_record {cr_name; cr_fields;}, doc ->
-           { (constructor_declaration ~loc ~res:None
-                ~name:(Located.mk ~loc cr_name)
-                ~args:(match kind_type_of_record_type_desc cr_fields with
-                    | Ptype_record fields -> Pcstr_record fields
-                    | _ -> failwith "impossible type_kind"))
-             with pcd_attributes = attributes_of_doc doc; })
-        constrs)
-
   let type_declaration_of_type_decl : type_decl -> type_declaration =
-    fun { td_name; td_kind=(kind, doc); } ->
+    fun { td_name=name; td_kind=(kind, doc); } ->
     let loc = Location.none in
+    let attributes_of_doc = function
+      | `docstr doc ->
+        let loc = Location.none in
+        [attribute ~loc
+           ~name:(Located.mk ~loc "ocaml.doc")
+           ~payload:(PStr [pstr_eval ~loc (estring ~loc doc) []])]
+      | `nodoc -> []
+      | _ -> failwith "unknown polymorphic variant for docstr" in
+    let kind_type_of_record_type_desc : record_type_desc -> type_kind = fun fields ->
+      Ptype_record (List.map (fun ({rf_name; rf_type;}, doc) ->
+          { (label_declaration ~loc ~mutable_:Immutable
+               ~name:(Located.mk ~loc rf_name)
+               ~type_:(ptyp_constr ~loc (Located.mk ~loc (lident rf_type)) []))
+            with pld_attributes = attributes_of_doc doc; })
+          fields) in
+    let kind_type_of_variant_type_desc : variant_type_desc -> type_kind = fun constrs ->
+      Ptype_variant (List.map (function
+          | Cstr_tuple {ct_name; ct_args;}, doc ->
+            { (constructor_declaration ~loc ~res:None
+                 ~name:(Located.mk ~loc ct_name)
+                 ~args:(Pcstr_tuple (List.map (fun ct_arg ->
+                     ptyp_constr ~loc (Located.mk ~loc (lident ct_arg)) [])
+                     ct_args)))
+              with pcd_attributes = attributes_of_doc doc; }
+          | Cstr_record {cr_name; cr_fields;}, doc ->
+            { (constructor_declaration ~loc ~res:None
+                 ~name:(Located.mk ~loc cr_name)
+                 ~args:(match kind_type_of_record_type_desc cr_fields with
+                     | Ptype_record fields -> Pcstr_record fields
+                     | _ -> failwith "impossible type_kind"))
+              with pcd_attributes = attributes_of_doc doc; })
+          constrs) in
     { (type_declaration ~loc ~params:[] ~cstrs:[] ~private_:Public ~manifest:None
-         ~name:(Located.mk ~loc td_name)
+         ~name:(Located.mk ~loc name)
          ~kind:(match kind with
              | Record_kind record -> kind_type_of_record_type_desc record
              | Variant_kind variant -> kind_type_of_variant_type_desc variant))
       with ptype_attributes = attributes_of_doc doc }
+
+  let gen_jsonm_encoder : string -> type_decl -> value_binding =
+    fun ec_name { td_name=_; td_kind=(kind, _); } ->
+    let loc = Location.none in
+    match kind with
+    | Record_kind record ->
+       let fields = List.map fst record in
+       let vari i = "__bindoj_record_field_var_"^string_of_int i in
+       let evari i = evar ~loc (vari i) in
+       let pvari i = pvar ~loc (vari i) in
+       let members = List.mapi (fun i { rf_name; rf_type; } ->
+           let name = [%expr `Name [%e estring ~loc rf_name]] in
+           if rf_type = "int" then
+             [%expr [[%e name]; `Float (float_of_int [%e evari i])]]
+           else if rf_type = "float" then
+             [%expr [[%e name]; `Float [%e evari i]]]
+           else if rf_type = "string" then
+             [%expr [[%e name]; `String [%e evari i]]]
+           else if rf_type = "bool" then
+             [%expr [[%e name]; `Bool [%e evari i]]]
+           else failwith "unsupported record field type") fields in
+       let obj = elist ~loc ([[%expr [`Os]]] @ members @ [[%expr [`Oe]]])
+                 |> fun e -> [%expr List.concat [%e e]] in
+       let rf_binds = List.mapi (fun i { rf_name; rf_type=_ } ->
+           (Located.mk ~loc (lident rf_name), pvari i)) fields in
+       value_binding ~loc
+         ~pat:(pvar ~loc ec_name)
+         ~expr:[%expr fun [%p ppat_record ~loc rf_binds Closed ] -> [%e obj]]
+    | Variant_kind _ ->
+       failwith "encoder of variant is not unimplemented."
 
 end
 
@@ -179,4 +204,8 @@ let () =
        [Datatype_desc.type_declaration_of_type_decl ex01_docstr]);
     (psig_type ~loc Recursive
        [Datatype_desc.type_declaration_of_type_decl ex02_docstr]);
-  ]
+  ];
+  Astlib.Pprintast.structure Format.std_formatter [
+    (pstr_value ~loc Nonrecursive
+       [Datatype_desc.gen_jsonm_encoder "encode_student_jsonm" ex01])
+  ];
