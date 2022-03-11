3a78db7872dd35cc80c964ec725b5eafbf3bdeff
hongoh <hg@kxc.io>
2022-03-11 18:01:33 +0900

    update tests

--- test/codegen_test.ml
+++ test/codegen_test.ml
@@ -1,14 +1,17 @@
 open Codegen
 
-let string_of_jsonm_seq jsonm_seq =
-  let buf = Buffer.create (Seq.fold_left (fun n _ -> n+1) 0 jsonm_seq) in
-  let encoder = Jsonm.encoder (`Buffer buf) in
-  Seq.iter (fun lxm -> let _ = Jsonm.encode encoder (`Lexeme lxm) in ()) jsonm_seq;
-  let _ = Jsonm.encode encoder `End in
-  String.of_bytes (Buffer.to_bytes buf)
+let jsonms_of_json_string json_string =
+  let decoder = Jsonm.decoder (`String json_string) in
+  let rec loop () =
+    match Jsonm.decode decoder with
+    | `Await -> loop ()
+    | `End -> []
+    | `Lexeme lxm -> lxm :: (loop ())
+    | `Error _ -> failwith "get invalid JSON string" in
+  loop ()
 
 let%test "ex01_encode" =
   let open Ex01_gen in
-  let s = { admission_year = 1984; name = "William Gibson"; } in
-  (string_of_jsonm_seq (encode_student_jsonm s))
-  = {|{"admission_year":1984,"name":"William Gibson"}|}
+  let ocaml_record = { admission_year = 1984; name = "William Gibson"; } in
+  let json_string = {|{"admission_year":1984,"name":"William Gibson"}|} in
+  encode_student_jsonm ocaml_record |> List.of_seq = jsonms_of_json_string json_string
