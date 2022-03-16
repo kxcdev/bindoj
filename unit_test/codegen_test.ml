3bf718bec3dd776aed7896e15b6dece9c492ba5b
hongoh <hg@kxc.io>
2022-03-16 20:54:51 +0900

    update unit_test

--- unit_test/codegen_test.ml
+++ unit_test/codegen_test.ml
@@ -11,8 +11,15 @@ let%test "ex01_encoding" =
   let jv_student : Json.jv =
     `obj [("admission_year", `num 1984.);
           ("name", `str "William Gibson")] in
-  let yojson_student : Yojson.Safe.t =
+  let yojson_student =
     `Assoc [("admission_year", `Int 1984);
             ("name", `String "William Gibson")] in
+  let jsonm_student : Jsonm.lexeme list =
+    [`Os;
+     `Name "admission_year"; `Float 1984.0;
+     `Name "name"; `String "William Gibson";
+     `Oe] in
   encoded_student = jv_student
-  && (Json.to_yojson encoded_student :> Yojson.Safe.t) = yojson_student
+  && Json.to_yojson encoded_student = yojson_student
+  && Json.to_yojson encoded_student |> Json.yojson_basic_of_safe = yojson_student
+  && Json.to_jsonm encoded_student |> List.of_seq = jsonm_student
