d269c4e280cc2b3ef10d58ceb28255ed2f89ca75
kxc-hongoh <hg@kxc.io>
2022-08-09 17:24:38 +0900

    remove comment out

--- scripts/ci_print_env.sh
+++ scripts/ci_print_env.sh
@@ -2,8 +2,8 @@
 echo ">>> uname -a"
 uname -a
 
-#echo && echo ">>> OCaml --version"
-#opam exec -- ocamlc --version
+echo && echo ">>> OCaml --version"
+opam exec -- ocamlc --version
 
 echo && echo ">>> git status"
 git show HEAD^..HEAD --stat || git show HEAD --stat
